ca5fcf057226bae05ca7bbafb9c72ad1096a8abb
Bao Nguyen <bao.nguyen2@trustingsocial.com>
2024-10-25 11:25:01 +0700

    add noSwaps for bubbleSort

--- sec11-bubble sort/index.js
+++ sec11-bubble sort/index.js
@@ -1,5 +1,7 @@
 const bubbleSort = (array) => {
+	let noSwaps;
   for (let i = array.length; i > 0; i--) {
+		noSwaps = true;
     for (let j = 0; j < i - 1; j++) {
       const eleJ = array[j];
       if (eleJ > array[j + 1]) {
@@ -7,8 +9,10 @@ const bubbleSort = (array) => {
         const temp = array[j];
         array[j] = array[j + 1];
         array[j + 1] = temp;
+				noSwaps = false;
       }
     }
+		if(noSwaps) break;
   }
   console.log('array: ', array);
   return array;
